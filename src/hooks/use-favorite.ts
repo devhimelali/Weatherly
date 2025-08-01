1a4ab0f98b750f584f86326cd8865f9716299cb0
devhimelali <dev.himelali@gmail.com>
2025-08-01 18:01:46 +0600

    Add `useFavorite` hook for managing favorite cities
    - Implement `useFavorite` to handle adding, removing, and checking favorite cities.
    - Integrate `useLocalStorage` and React Query for state persistence and caching.
    - Support a maximum of 10 favorite cities with duplicate prevention.

---
+++ src/hooks/use-favorite.ts
@@ -0,0 +1,72 @@
+import {useLocalStorage} from "@/hooks/use-local-storage.ts";
+import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
+
+interface FavoriteCity {
+    id: string,
+    name: string,
+    lat: number,
+    lon: number,
+    country: string,
+    state?: string,
+    addedAt: number
+}
+
+export function useFavorite() {
+    const [favorites, setFavorites] = useLocalStorage<FavoriteCity[]>("favorites", []);
+
+    const queryClient = useQueryClient();
+
+    const favoritesQuery = useQuery({
+        'queryKey': ['favorites'],
+        queryFn: () => favorites,
+        initialData: favorites,
+        staleTime: Infinity
+    });
+
+    const addToFavorite = useMutation({
+        mutationFn: async (city: Omit<FavoriteCity, "id" | "addedAt">) => {
+            const newFavorite: FavoriteCity = {
+                ...city,
+                id: `${city.lat}-${city.lon}-${Date.now()}`,
+                addedAt: Date.now(),
+            };
+
+            const exists = favorites.some((fav) => fav.id === newFavorite.id);
+
+            if (exists) {
+                return favorites;
+            }
+
+            const newFavorites = [...favorites, newFavorite].slice(0, 10);
+
+            setFavorites(newFavorites);
+
+            return newFavorites;
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ["favorites"]
+            })
+        }
+    });
+
+    const removeFromFavorite = useMutation({
+        mutationFn: async (id: string) => {
+            const newFavorites = favorites.filter((fav) => fav.id !== id);
+            setFavorites(newFavorites);
+            return newFavorites;
+        },
+        onSuccess: () => {
+            queryClient.invalidateQueries({
+                queryKey: ["favorites"]
+            })
+        }
+    });
+
+    return {
+        favorites: favoritesQuery.data,
+        addToFavorite,
+        removeFromFavorite,
+        isFavorite: (lat: number, lon: number) => favorites.some((city) => city.lat === lat && city.lon === lon)
+    }
+}
